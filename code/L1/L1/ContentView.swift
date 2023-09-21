44e1c10829d095b532ede0d91e54576b1824d946
zhengweix <zheng.wei@famewell.com>
2023-09-20 21:44:36 -0700

    updated 230920

---
+++ code/L1/L1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  L1
+//
+//  Created by Wei Zheng on 2023/9/20.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        NavigationStack {
+            Text("Hello, VisionOS")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ code/L1/L1/L1App.swift
@@ -0,0 +1,17 @@
+//
+//  L1App.swift
+//  L1
+//
+//  Created by Wei Zheng on 2023/9/20.
+//
+
+import SwiftUI
+
+@main
+struct L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
