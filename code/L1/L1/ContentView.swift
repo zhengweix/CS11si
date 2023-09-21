2f147f03524878672f6ab53cb52a21f02a3bf243
zhengweix <zheng.wei@famewell.com>
2023-09-20 23:39:42 -0700

    updated 230920

--- code/L1/L1/ContentView.swift
+++ code/L1/L1/ContentView.swift
@@ -10,10 +10,25 @@ import RealityKit
 
 struct ContentView: View {
 
+    private var url = URL(string: "https://developer.apple.com/augmented-reality/quick-look/models/teapot/teapot.usdz")!
+    
     var body: some View {
         NavigationStack {
-            Text("Hello, VisionOS")
+//            Text("Hello, VisionOS")
+            VStack {
+                Text("Show Teapot")
+                
+                Model3D(url: url) { model in
+                    model
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
         }
+        .padding()
     }
 }
 
