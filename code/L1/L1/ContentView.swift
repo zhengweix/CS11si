7e10839e0464e8a6ccf32b58b56da7bab41934e3
zhengweix <zheng.wei@famewell.com>
2023-09-22 00:48:38 -0700

    updated 230922

---
+++ code/L1/L1/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  L1
+//
+//  Created by Wei Zheng on 2023/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ code/L1/L1/ImmersiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ImmersiveView.swift
+//  L1
+//
+//  Created by Wei Zheng on 2023/9/22.
+//
+
+import SwiftUI
+
+struct ImmersiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImmersiveView()
+}
