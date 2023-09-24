c189e27ac80be489fcce98ec36d950f61809f2c6
zhengweix <zheng.wei@famewell.com>
2023-09-23 18:11:04 -0700

    updated 230923

--- code/WallArt/WallArt/ImmersiveView.swift
+++ code/WallArt/WallArt/ImmersiveView.swift
@@ -10,17 +10,60 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    
+    @State var characterEntity: Entity = {
+        let headAnchor = AnchorEntity(.head)
+        headAnchor.position = [0.70, -0.35, -1]
+        let radians = -30 * Float.pi / 180
+        ImmersiveView.rotateEntityAroundYAxis(entity: headAnchor, angle: radians)
+        return headAnchor
+    }()
+    
+    @State var planeEntity: Entity = {
+        let wallAnchor = AnchorEntity(.plane(.vertical, classification: .wall, minimumBounds: SIMD2<Float>(0.6, 0.6)))
+        let planeMesh = MeshResource.generatePlane(width: 1.43, depth: 1.0, cornerRadius: 0.1)
+        let material = ImmersiveView.loadImageMaterial(imageUrl: "think_different")
+        let planeEntity = ModelEntity(mesh: planeMesh, materials: [material])
+        planeEntity.name = "canvas"
+        wallAnchor.addChild(planeEntity)
+        return wallAnchor
+    }()
+    
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
+            do {
+                let immersiveEntity = try await Entity(named: "Immersive", in: realityKitContentBundle)
+                characterEntity.addChild(immersiveEntity)
+                content.add(characterEntity)
+                content.add(planeEntity)
+            } catch {
+                print("Error in RealityView's make: \(error)")
             }
         }
     }
-}
+    
+    static func rotateEntityAroundYAxis(entity: Entity, angle: Float) {
+        // Get the current transform of the entity
+        var currentTransform = entity.transform
 
-#Preview {
-    ImmersiveView()
-        .previewLayout(.sizeThatFits)
+        // Create a quaternion representing a rotation around the Y-axis
+        let rotation = simd_quatf(angle: angle, axis: [0, 1, 0])
+
+        // Combine the rotation with the current transform
+        currentTransform.rotation = rotation * currentTransform.rotation
+
+        // Apply the new transform to the entity
+        entity.transform = currentTransform
+    }
+    static func loadImageMaterial(imageUrl: String) -> SimpleMaterial {
+        do {
+            let texture = try TextureResource.load(named: imageUrl)
+            var material = SimpleMaterial()
+            let color = SimpleMaterial.BaseColor(texture: MaterialParameters.Texture(texture))
+            material.color = color
+            return material
+        } catch {
+            fatalError(String(describing: error))
+        }
+    }
 }
